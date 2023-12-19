cfefb703d8c6fb5d11e19d2d4c25b443286eec26
Marco <marcoregina92@gmail.com>
2023-12-19 17:02:00 +0100

    inserisce filtro hotel con bug da risolvere

--- alberghi.php
+++ alberghi.php
@@ -1,3 +1,8 @@
+
+<?php
+$voto = $_GET["vote"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Trova il tuo Hotel!</title>
 </head>
+
+
 <!-- Array di hotels -->
 <?php
 
@@ -68,24 +75,28 @@
   <?php
     
     foreach ($hotels as $hotel) {
-        echo "<tr>";
-            echo "<td>" . $hotel["name"] . "</td>";
-            echo "<td>" . $hotel["description"] . "</td>";
-            if ($hotel["parking"] == true) {
-                echo "<td> Si </td>";
-            } else {
-                echo "<td> No </td>";
-
-            }
-            echo "<td>" . $hotel["vote"] . "</td>";
-            echo "<td>" . $hotel["distance_to_center"] . " km" . "</td>";
-        echo "</tr>";
+        if ($voto == $hotel["vote"]) {
+            echo "<tr>";
+                echo "<td>" . $hotel["name"] . "</td>";
+                echo "<td>" . $hotel["description"] . "</td>";
+                if ($hotel["parking"] == true) {
+                    echo "<td> Si </td>";
+                } else {
+                    echo "<td> No </td>";
+    
+                }
+                echo "<td>" . $hotel["vote"] . "</td>";
+                echo "<td>" . $hotel["distance_to_center"] . " km" . "</td>";
+            echo "</tr>";
+        }
 
     }
 
     ?>
   </tbody>
 </table>
+
+<a href="ricerca.php"> Torna alla ricerca hotel per voto </a>
 
 </body>
 </html>
---
+++ ricerca.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtra Hotels per voto</title>
+</head>
+<body>
+    <form action="alberghi.php">
+        <input type="number" name="vote" min="1" max="5" name="vote">
+        <button type="submit"> Cerca </button>
+    </form>
+</body>
+</html>
